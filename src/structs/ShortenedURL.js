1816807d100842bd973f9dd33358f0d546b1135a
Kai Cao <ImAffinix@gmail.com>
2018-07-16 17:51:56 +1000

    clean up shortenedURL schema

--- src/structs/ShortenedURL.js
+++ src/structs/ShortenedURL.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-const ShortenedURL = new mongoose.Schema({
-  url: { type: String, required: true },
-  id: { type: String, required: true }
-});
+const ShortenedURL = new mongoose.Schema({ url: { type: String, required: true }, id: { type: String, required: true } });
 
 module.exports = mongoose.model("ShortenedURL", ShortenedURL);
