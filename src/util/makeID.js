c73413ea202e7e059ceafa128dd92ea53b722189
Kai Cao <ImAffinix@gmail.com>
2018-07-17 07:22:42 +1000

    Update makeID.js

--- src/util/makeID.js
+++ src/util/makeID.js
@@ -2,7 +2,8 @@ function makeid() {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 5; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
+  for (var i = 0; i < 5; i++) 
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
 }
